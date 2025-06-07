72862d7e069fefc7e5ee3c51be5e89f503c7f618
Vitor Paulino <vitorpaulinog@gmail.com>
2025-06-07 16:43:22 -0300

    move: Application class to correct dir and rename to FilmesApplication

--- src/test/java/com/programacaoweb/filmes/FilmesApplicationTests.java
+++ src/test/java/com/programacaoweb/filmes/FilmesApplicationTests.java
@@ -1,10 +1,10 @@
-package filme.com;
+package com.programacaoweb.filmes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ComApplicationTests {
+class FilmesApplicationTests {
 
 	@Test
 	void contextLoads() {
