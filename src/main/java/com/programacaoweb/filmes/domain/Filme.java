bbe8b258804db9abf6a55c44d644222dcdaf43d7
Vitor Paulino <vitorpaulinog@gmail.com>
2025-06-08 12:26:44 -0300

    Add: filme entity

---
+++ src/main/java/com/programacaoweb/filmes/domain/Filme.java
@@ -0,0 +1,33 @@
+package com.programacaoweb.filmes.domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Filme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "O título deve ser preenchido")
+    private String title;
+    @NotBlank(message = "A descrição deve ser preenchida")
+    private String description;
+    @NotNull
+    private Float price;
+    @NotBlank
+    private String director;
+    private LocalDate isDeleted;
+    private String imageUrl;
+}
