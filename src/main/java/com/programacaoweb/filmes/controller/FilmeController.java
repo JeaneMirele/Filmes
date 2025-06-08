562e43fd2530db95cc01a36840c592da19ceafb4
JeaneMirele <jeanemirele7@gmail.com>
2025-06-08 16:24:00 -0300

    Card

--- src/main/java/com/programacaoweb/filmes/controller/FilmeController.java
+++ src/main/java/com/programacaoweb/filmes/controller/FilmeController.java
@@ -1,13 +1,25 @@
 package com.programacaoweb.filmes.controller;
 
-import ch.qos.logback.core.model.Model;
+
+import com.programacaoweb.filmes.domain.Filme;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FilmeController {
     @GetMapping
     public String index(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model){
+        Filme filme = new Filme(1L,"A viagem de Chihiro", "Chihiro e seus pais estão se mudando para uma cidade diferente. A caminho da nova casa, o pai decide pegar um atalho. Eles se deparam com uma mesa repleta de comida, embora ninguém esteja por perto. Chihiro sente o perigo, mas seus pais começam a comer. Quando anoitece, eles se transformam em porcos. Agora, apenas Chihiro pode salvá-los.", "Ficção científica", 39.90f,
+                "Hayao Miyazaki",null, "/img/Chihiro.jpg");
+
+        model.addAttribute("filme", filme);
+        model.addAttribute("count", 1);
         return "index";
     }
 }
